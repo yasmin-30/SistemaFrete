ba0650d0b0b5b38cfea0d256470d5854b2591f67
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-09-30 17:40:42 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -1,11 +1,12 @@
 package sistemaFrete;
 
 public abstract class Veiculo {
-	String placa;
-	int ano;
+	private String placa;
+	private int ano;
 	
 	public Veiculo(String placa, int ano){
 		this.placa = placa;
 		this.ano = ano;
 	}
 }
+
